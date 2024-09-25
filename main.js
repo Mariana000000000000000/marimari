f34f88264f4b252f90b75d44fc620f685bcd7666
Mari <142262838+Mariana000000000000000@users.noreply.github.com>
2024-09-25 11:42:47 -0300

    gnjj

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function soma(num1, num2) {
 }
 console.log(soma(3, 4));
 
-function multiplication(num1*num2) {
+function multiplication(num1,num2) {
    return num1*num2;
 
- console.log(soma(l*l))
+ console.log(soma(l*l));
 }
