913f353d24e2acd5848ac1714315d02999bd038d
Mari <142262838+Mariana000000000000000@users.noreply.github.com>
2024-09-25 11:41:11 -0300

    wefw

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 alert('Mariana Alves')
 
-function soma (num1, num2) {
-    return num1+num2; 
+function soma(num1, num2) {
+    return num1 + num2;
 }
-console.log(soma (3,4));
-console.log(soma (l*l=l²))
+console.log(soma(3, 4));
+
+function multiplication(num1*num2) {
+   return num1*num2;
+
+ console.log(soma(l*l))
+}
